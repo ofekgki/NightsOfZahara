06736b81cf77f819188cd5e6f35650140bb379ee
Ofek Kanari <ofekgki@gmail.com>
2026-07-07 02:43:20 +0300

    Initial Commit

---
+++ NightsOfZahara/NightsOfZaharaApp.swift
@@ -0,0 +1,17 @@
+//
+//  NightsOfZaharaApp.swift
+//  NightsOfZahara
+//
+//  Created by Ofek Kanari on 07/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct NightsOfZaharaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
